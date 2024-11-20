cd9190808bd32f868222d026c4ad1f0e1f1315ef
user-matth <mathuscardoso@gmail.com>
2024-11-20 15:34:57 -0300

    wip: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,6 +4,16 @@ export default async function sitemap() {
       url: "https://www.geradoor.com",
       lastModified: new Date(),
       priority: 1
-    }
+    },
+    {
+      url: "https://www.geradoor.com/qr-code",
+      lastModified: new Date(),
+      priority: 0.8
+    },
+    {
+      url: "https://www.geradoor.com/whatsapp",
+      lastModified: new Date(),
+      priority: 0.8
+    },
   ]
 }
