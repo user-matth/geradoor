0f97f96b4f931b580cb3da6c96c5d1198b059134
user-matth <mathuscardoso@gmail.com>
2024-11-20 15:29:38 -0300

    wip responsiveness

--- src/app/whatsapp/page.tsx
+++ src/app/whatsapp/page.tsx
@@ -150,7 +150,7 @@ const WhatsappLinkGenerator: React.FC = () => {
       <NavbarSection />
 
       <motion.div
-        className="max-w-screen-md mx-auto w-full h-full max-h-[400px] flex flex-col space-y-2 items-start justify-center"
+        className="max-w-screen-md mx-auto w-full h-full max-h-[400px] hidden md:flex lg:flex flex-col space-y-2 items-start justify-center"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.4 }}
@@ -273,6 +273,71 @@ const WhatsappLinkGenerator: React.FC = () => {
             </motion.div>
           </ResizablePanel>
         </ResizablePanelGroup>
+      </motion.div>
+
+      <motion.div
+        className="max-w-screen-sm mx-auto w-full h-full max-h-[400px] flex md:hidden lg:hidden flex-col space-y-2 items-start justify-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+      >
+        <div className="flex flex-col h-full justify-center w-full mx-auto px-4 max-w-[400px]">
+          <div className="font-semibold leading-none tracking-tight mb-1">Compatilhe links de WhatsApp</div>
+          <div className="text-sm text-muted-foreground mb-6">Digite o número de telefone do WhatsApp que quer criar.</div>
+          <PhoneInput defaultCountry="BR" value={phone ?? ""} onChange={(e) => setPhone(e)} placeholder="(00) 00000-0000" />
+          <Input
+            type="text"
+            placeholder="Customize sua mensagem"
+            className="bg-background mt-4"
+            value={message ?? ""}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <p className="mt-2 font-normal text-sm tracking-tight leading-4">Exemplo: &quot;Olá, eu gostaria de receber mais informações sobre o produto&quot;</p>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="w-full mt-8" onClick={handleGenerate} disabled={phone == ""}>
+                Gerar meu link
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Esse é o seu link do WhatsApp</DialogTitle>
+                <DialogDescription>
+                  Copie e compartilhe em qualquer lugar para ser contactado instantaneamente.
+                </DialogDescription>
+                { loadingQrCode ? (
+                  <div className="flex items-center justify-center w-full h-full py-12">
+                    <Loader className="animate-spin rounded-full h-5 w-5"/>
+                  </div>
+                ) : (
+                  <div className="flex flex-col pt-6">
+                    <div className="flex flex-row items-center justify-center mb-6 mx-auto">
+                      <Link className="underline underline-offset-4" href={qrCodeValue ?? ""} target="_blank" >{qrCodeValue}</Link>
+                      <Button variant="ghost" className="w-7 h-7 p-0 ms-1" onClick={copyLink}>
+                        <Copy />
+                      </Button>
+                    </div>
+                    <QRCodeSVG
+                      className="mx-auto qrcode-svg"
+                      value={qrCodeValue ?? ""}
+                      size={200}
+                      fgColor={theme === "dark" ? "white" : "black"}
+                      bgColor={theme === "dark" ? "black" : "white"}
+                    />
+                    <div className="flex flex-row gap-4 mt-6 justify-center">
+                      <Button variant="secondary" onClick={downloadQRCode}>
+                        Baixar QR Code
+                      </Button>
+                      <Button className="" onClick={copyLink}>
+                        Copiar link
+                      </Button>
+                    </div>
+                  </div>
+                ) }
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </div>
       </motion.div>
 
       <FooterSection toggleTheme={toggleTheme} />
--- src/components/NavbarSection.tsx
+++ src/components/NavbarSection.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { DoorOpen } from 'lucide-react';
+import { DoorOpen, Minus } from 'lucide-react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import {
@@ -16,6 +16,7 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { usePathname } from 'next/navigation';
+import { Separator } from './ui/separator';
 
 const NavbarSection: React.FC = () => {
   const currentPath = usePathname();
@@ -32,7 +33,7 @@ const NavbarSection: React.FC = () => {
           <DoorOpen className="w-4 h-4" />
           Geradoor
         </Button>
-        <div className="mx-auto">
+        <div className="mx-auto hidden lg:inline md:inline">
           <Button variant="ghost" asChild>
             <Link href={'/'} className={currentPath === '/' ? '' : 'text-zinc-500'}>
               CPF
@@ -49,12 +50,52 @@ const NavbarSection: React.FC = () => {
             </Link>
           </Button>
         </div>
-        <Button variant="link" asChild>
+        <Drawer>
+          <DrawerTrigger className='flex lg:hidden md:hidden ms-auto me-2'>
+            <div className="flex flex-col space-y-1.5 h-8 w-8 items-center justify-center">
+              <div className="w-5 h-[1px] p-0 bg-primary rounded-full"></div>
+              <div className="w-5 h-[1px] p-0 bg-primary rounded-full"></div>
+            </div>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Menu</DrawerTitle>
+              {/* <DrawerDescription>Entenda nossa intenção</DrawerDescription> */}
+            </DrawerHeader>
+            <div className="flex flex-col px-4 pb-12 space-y-2">
+              <Link href={'/'} className={currentPath === '/' ? '' : 'text-zinc-500'}>
+                CPF
+              </Link>
+              <Link href={'/qr-code'} className={currentPath === '/qr-code' ? '' : 'text-zinc-500'}>
+                QRCode
+              </Link>
+              <Link href={'/whatsapp'} className={currentPath === '/whatsapp' ? '' : 'text-zinc-500'}>
+                Whatsapp
+              </Link>
+              { currentPath === "/" && (
+                <div className="flex flex-col space-y-4 text-sm">
+                  <Separator className='my-6'/>
+                  <p>Nosso gerador online de CPF tem como intenção ajudar estudantes, programadores, analistas e testadores a gerar CPF válidos. Normalmente necessários para testar seus softwares em desenvolvimento.</p>
+                  <p>A má utilização dos dados aqui gerados é de total responsabilidade do usuário.</p>
+                  <p>Os números são gerados de forma aleatória, e não salvamos nenhum CPF gerado em nossa base, respeitando as regras de criação de cada documento.</p>
+                </div>
+              ) }
+            </div>
+            {/* <DrawerFooter>
+              <Button>Submit</Button>
+              <DrawerClose>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter> */}
+          </DrawerContent>
+        </Drawer>
+
+        <Button variant="link" asChild className='hidden lg:inline md:inline'>
           <Link href={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target='_blank' className="ms-auto text-zinc-400">
             API
           </Link>
         </Button>
-        { currentPath === "/" ? (
+        {/* { currentPath === "/" ? (
           <Drawer>
             <DrawerTrigger asChild>
               <Button variant="link" className='h-8 lg:hidden md:hidden flex'>
@@ -80,7 +121,7 @@ const NavbarSection: React.FC = () => {
               </div>
             </DrawerContent>
           </Drawer>
-        ) : null}
+        ) : null} */}
       </div>
     </motion.div>
   );
